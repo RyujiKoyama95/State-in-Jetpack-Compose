470957aa08b926c9c2f59d589920b99df44078ea
DESKTOP-9REHMCV\koyama ryuji <ryujikoyama95@gmail.com>
2023-08-02 22:39:50 +0900

    Add WaterCounter screen

---
+++ app/src/main/java/com/example/stateinjetpackcompose/WaterCounter.kt
@@ -0,0 +1,17 @@
+package com.example.stateinjetpackcompose
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+// 再利用しやすくするため、コンポーザブルにデフォルトのModifierを指定するのがいい
+@Composable
+fun WaterCounter(modifier: Modifier = Modifier) {
+    val count = 0
+    Text(
+        text = "You've had $count glasses.",
+        modifier = modifier.padding(16.dp)
+    )
+}
