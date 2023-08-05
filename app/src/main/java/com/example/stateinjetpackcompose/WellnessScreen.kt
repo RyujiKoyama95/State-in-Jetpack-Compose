7f9a6cf85d1c1e7bace25edd51198fb0e1ba4e05
DESKTOP-9REHMCV\koyama ryuji <ryujikoyama95@gmail.com>
2023-08-05 17:44:06 +0900

    Update wellnessTask to observable mutableList

--- app/src/main/java/com/example/stateinjetpackcompose/WellnessScreen.kt
+++ app/src/main/java/com/example/stateinjetpackcompose/WellnessScreen.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
@@ -14,6 +16,17 @@ fun WellnessScreen(modifier: Modifier = Modifier) {
     ) {
         StatefulCounter()
         Spacer(modifier = Modifier.padding(10.dp))
-        WellnessTasksList()
+
+        val list = remember { getWellnessTasks().toMutableStateList() }
+        WellnessTasksList(list = list)
+    }
+}
+
+/**
+ *  Create dummy data
+ */
+private fun getWellnessTasks(): List<WellnessTask> {
+    return List(30) { i ->
+        WellnessTask(i, "Task #$i")
     }
 }
--- app/src/main/java/com/example/stateinjetpackcompose/WellnessTasksList.kt
+++ app/src/main/java/com/example/stateinjetpackcompose/WellnessTasksList.kt
@@ -3,26 +3,16 @@ package com.example.stateinjetpackcompose
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 
 @Composable
 fun WellnessTasksList(
     modifier: Modifier = Modifier,
-    list: List<WellnessTask> = remember { getWellnessTasks() }
+    list: List<WellnessTask>
 ) {
     LazyColumn(content = {
         items(list) {
             WellnessTaskItem(taskName = it.label)
         }
     })
-}
-
-/**
- *  Create dummy data
- */
-private fun getWellnessTasks(): List<WellnessTask> {
-    return List(30) { i ->
-        WellnessTask(i, "Task #$i")
-    }
 }
