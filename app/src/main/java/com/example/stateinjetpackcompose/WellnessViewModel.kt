6ceed370d026465abfba395497df27ed3b57a7af
DESKTOP-9REHMCV\koyama ryuji <ryujikoyama95@gmail.com>
2023-08-06 07:03:51 +0900

    Add WellnessViewModel for saving UI state

---
+++ app/src/main/java/com/example/stateinjetpackcompose/WellnessViewModel.kt
@@ -0,0 +1,22 @@
+package com.example.stateinjetpackcompose
+
+import androidx.compose.runtime.toMutableStateList
+import androidx.lifecycle.ViewModel
+
+class WellnessViewModel: ViewModel() {
+    private val _tasks = getWellnessTasks().toMutableStateList()
+    val tasks: List<WellnessTask> = _tasks
+
+    fun remove(item: WellnessTask) {
+        _tasks.remove(item)
+    }
+}
+
+/**
+ *  Create dummy data
+ */
+private fun getWellnessTasks(): List<WellnessTask> {
+    return List(30) { i ->
+        WellnessTask(i, "Task #$i")
+    }
+}
