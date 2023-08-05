409637fed744ccd294bc5a69587b398d5b07febf
DESKTOP-9REHMCV\koyama ryuji <ryujikoyama95@gmail.com>
2023-08-06 08:28:40 +0900

    Cleanup import

--- app/src/main/java/com/example/stateinjetpackcompose/WaterCounter.kt
+++ app/src/main/java/com/example/stateinjetpackcompose/WaterCounter.kt
@@ -1,6 +1,5 @@
 package com.example.stateinjetpackcompose
 
-import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
@@ -9,7 +8,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
--- app/src/main/java/com/example/stateinjetpackcompose/WellnessScreen.kt
+++ app/src/main/java/com/example/stateinjetpackcompose/WellnessScreen.kt
@@ -4,8 +4,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
