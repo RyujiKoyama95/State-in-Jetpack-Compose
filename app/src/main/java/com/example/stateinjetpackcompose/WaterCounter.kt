5e5ae9e376647a15cb1221a91dcb85e6699ee474
DESKTOP-9REHMCV\koyama ryuji <ryujikoyama95@gmail.com>
2023-08-05 09:50:14 +0900

    Add rememberSaveable

--- app/src/main/java/com/example/stateinjetpackcompose/WaterCounter.kt
+++ app/src/main/java/com/example/stateinjetpackcompose/WaterCounter.kt
@@ -10,6 +10,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -18,8 +19,9 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        // rememberは再コンポーズ後は状態を保持するが、設定の変更豪(Activityの再作成)は保持されない
-        var count by remember { mutableStateOf(0) }
+        // rememberは再コンポーズ後は状態を保持するが、設定の変更後(Activityやプロセスの再作成後)は保持されない
+        // 保持されるようにするにはrememberSaveableを使用する
+        var count by rememberSaveable{ mutableStateOf(0) }
         if (count > 0) {
             // WellnessTaskItem#onClose()が走り、showTask=falseの状態で、Button#count=0→Button#count++が走ると、showTask=trueに再初期化される
             // これは、Button#count=0が走ると、再コンポジションが発生するが、val showTaskのコードが呼び出されなかったため、showTaskの状態は破棄される
